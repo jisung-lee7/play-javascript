0f53b969f68f4b0c7211bbba78b6b21e81071f3e
Jisung Lee <jskurt9352@gmail.com>
2024-01-30 00:12:32 +0900

    add callback-to-promise.js

---
+++ async/callback-to-promise.js
@@ -0,0 +1,36 @@
+'use strict'
+// Callback Hell example
+class UserStorage {
+  loginUser(id, password) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (id === 'jisung' && password === 'lee') {
+          resolve(id)
+        } else {
+          reject(new Error('not found'))
+        }
+      }, 2000)
+    })
+  }
+
+  getRoles(user) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (user === 'jisung') {
+          resolve({ name: 'jisung', role: 'admin' })
+        } else {
+          reject(new Error('no access'))
+        }
+      }, 1000)
+    })
+  }
+}
+const userStorage = new UserStorage()
+const id = prompt('enter your id')
+const password = prompt('enter your password')
+
+userStorage
+  .loginUser(id, password)
+  .then(userStorage.getRoles)
+  .then((user) => alert(`Hello ${user.name}, you have a ${user.role} role`))
+  .catch(console.log)

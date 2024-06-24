29a3986b8a4243fb5e5aee7c3e1e4d65f801c6b8
Jisung Lee <jskurt9352@gmail.com>
2024-06-24 14:18:57 +0900

    update function.js

--- function.js
+++ function.js
@@ -16,18 +16,21 @@
 */
 console.log(`\n[ Function(Function Declaration) ]`)
 
-function printHello() {
-  console.log(`Hello`)
+{
+  function printHello() {
+    console.log(`Hello`)
+  }
+
+  printHello()
 }
 
-printHello()
+{
+  function log(message) {
+    console.log(message)
+  }
 
-function log(message) {
-  console.log(message)
+  log('Hello')
 }
-
-log('Hello')
-
 /**
  ****************************************************************************************************
  * ### Parameters
@@ -37,12 +40,14 @@ log('Hello')
  */
 console.log(`\n[ Parameters ]`)
 
-function changeName(obj) {
-  obj.name = 'JisungLee'
+{
+  function changeName(obj) {
+    obj.name = 'JisungLee'
+  }
+  const jisung = { name: 'jisung' }
+  changeName(jisung)
+  console.log(jisung)
 }
-const jisung = { name: 'jisung' }
-changeName(jisung)
-console.log(jisung)
 
 /**
  ****************************************************************************************************
@@ -51,10 +56,12 @@ console.log(jisung)
  */
 console.log(`\n[ Default parameters (added in ES6) ]`)
 
-function showMessage(message, from = 'unknown') {
-  console.log(`${message} by ${from}`)
+{
+  function showMessage(message, from = 'unknown') {
+    console.log(`${message} by ${from}`)
+  }
+  showMessage('Hi')
 }
-showMessage('Hi')
 
 /**
  ****************************************************************************************************
@@ -67,13 +74,16 @@ showMessage('Hi')
  */
 console.log(`\n[ Arguments ]`)
 
-function showName(name) {
-  console.log(`c.log ## arguments ##`, arguments)
-  console.log(`c.log ## arguments.length ##`, arguments.length)
-  console.log(`c.log ## arguments[0] ##`, arguments[0])
-  console.log(`c.log ## arguments[1] ##`, arguments[1])
+{
+  function showName(name) {
+    console.log(`c.log ## arguments ##`, arguments)
+    console.log(`c.log ## arguments.length ##`, arguments.length)
+    console.log(`c.log ## arguments[0] ##`, arguments[0])
+    console.log(`c.log ## arguments[1] ##`, arguments[1])
+  }
+
+  showName('Jisung', 'Jenny')
 }
-showName('Jisung', 'Jenny')
 
 /**
  ****************************************************************************************************
@@ -83,16 +93,18 @@ showName('Jisung', 'Jenny')
  */
 console.log(`\n[ Rest parameters (added in ES6) ]`)
 
-function printAll(...args) {
-  for (let i = 0; i < args.length; i++) {
-    console.log(`c.log ## args[i] ##`, args[i])
-  }
+{
+  function printAll(...args) {
+    for (let i = 0; i < args.length; i++) {
+      console.log(`c.log ## args[i] ##`, args[i])
+    }
 
-  for (const arg of args) {
-    console.log(`c.log ## arg ##`, arg)
+    for (const arg of args) {
+      console.log(`c.log ## arg ##`, arg)
+    }
   }
+  printAll('js', 'kurt', 'JisungLee')
 }
-printAll('js', 'kurt', 'JisungLee')
 
 /**
  ****************************************************************************************************
@@ -104,20 +116,22 @@ printAll('js', 'kurt', 'JisungLee')
  */
 console.log(`\n[ Function expression ]`)
 
-function sum(a, b) {
-  return a + b
-}
-const print = function () {
-  // Anonymous function
-  console.log(`print`)
-}
-print()
+{
+  function sum(a, b) {
+    return a + b
+  }
+  const print = function () {
+    // Anonymous function
+    console.log(`print`)
+  }
+  print()
 
-const printAgain = print
-printAgain()
+  const printAgain = print
+  printAgain()
 
-const sumAgain = sum
-console.log(sumAgain(1, 3))
+  const sumAgain = sum
+  console.log(sumAgain(1, 3))
+}
 
 /**
  ****************************************************************************************************
@@ -126,27 +140,29 @@ console.log(sumAgain(1, 3))
  */
 console.log(`\n[ Callback function using function expression ]`)
 
-function randomQuiz(answer, yesCallback, noCallback) {
-  if (answer === 'helloworld') {
-    yesCallback()
-  } else {
-    noCallback()
+{
+  function randomQuiz(answer, yesCallback, noCallback) {
+    if (answer === 'helloworld') {
+      yesCallback()
+    } else {
+      noCallback()
+    }
   }
-}
 
-// Anonymous function
-const printYes = function () {
-  console.log(`yes!`)
-}
+  // Anonymous function
+  const printYes = function () {
+    console.log(`yes!`)
+  }
 
-// named function
-// better debugging in debugger's stack traces
-const printNo = function print() {
-  console.log(`no!`)
-}
+  // named function
+  // better debugging in debugger's stack traces
+  const printNo = function print() {
+    console.log(`no!`)
+  }
 
-randomQuiz('wrong', printYes, printNo)
-randomQuiz('helloworld', printYes, printNo)
+  randomQuiz('wrong', printYes, printNo)
+  randomQuiz('helloworld', printYes, printNo)
+}
 
 /**
  ****************************************************************************************************
@@ -161,32 +177,171 @@ randomQuiz('helloworld', printYes, printNo)
  */
 console.log(`\n[ Arrow function ]`)
 
-const simplePrint = () => console.log(`simplePrint!`)
-const add = (a, b) => a + b
-const simpleMultiply = (a, b) => {
-  // do something more
-  return a * b
+{
+  const simplePrint = () => console.log(`simplePrint!`)
+  const add = (a, b) => a + b
+  const simpleMultiply = (a, b) => {
+    // do something more
+    return a * b
+  }
+
+  // DON'T USE arrow function in object method
+  {
+    let functionObj = {
+      name: 'jisung',
+      sayThis: function () {
+        console.log(`c.log ## this ##`, this)
+      }
+    }
+
+    functionObj.sayThis() // return this object
+
+    let arraowFunctionObj = {
+      name: 'jisung',
+      sayThis: () => {
+        console.log(`c.log ## this ##`, this)
+      }
+    }
+
+    arraowFunctionObj.sayThis() // return window
+  }
 }
 
-// DON'T USE arrow function in object method
+/**
+ ****************************************************************************************************
+ * ### Call, Apply, Bind
+ ****************************************************************************************************
+ * Call
+ *   - The 'call' method can be used on any function and allows you to set 'this' to a specific value.
+ * Apply
+ *   - The apply method is similar to call, but it takes arguments as an array.
+ * Bind
+ *   - You can permanently change the this value of a function.
+ */
+console.log(`\n[ Call, Apply, Bind ]`)
+
+// call
 {
-  let functionObj = {
-    name: 'jisung',
-    sayThis: function () {
-      console.log(`c.log ## this ##`, this)
+  const jisung = {
+    name: 'Jisung'
+  }
+
+  const jenny = {
+    name: 'Jenny'
+  }
+
+  function showThisName() {
+    console.log(`c.log ## this.name ##`, this.name)
+  }
+
+  // showThisName() // error
+  showThisName.call(jisung)
+  showThisName.call(jenny)
+
+  function update(birthYear, occupation) {
+    this.birthYear = birthYear
+    this.occupation = occupation
+  }
+
+  update.call(jisung, 1999, 'engineer')
+  console.log(`c.log ## use call jisung ##`, jisung)
+
+  update.call(jenny, 2000, 'actor')
+  console.log(`c.log ## use call jenny ##`, jenny)
+}
+
+// apply
+{
+  const jisung = {
+    name: 'Jisung'
+  }
+
+  const jenny = {
+    name: 'Jenny'
+  }
+
+  function update(birthYear, occupation) {
+    this.birthYear = birthYear
+    this.occupation = occupation
+  }
+
+  update.apply(jisung, [1999, 'engineer'])
+  console.log(`c.log ## use apply jisung ##`, jisung)
+
+  update.apply(jenny, [2000, 'actor'])
+  console.log(`c.log ## use apply jenny ##`, jenny)
+}
+
+{
+  const minNum = Math.min(3, 10, 1, 6, 4)
+  const maxNum = Math.max(3, 10, 1, 6, 4)
+
+  console.log(`c.log ## minNum ##`, minNum)
+  console.log(`c.log ## maxNum ##`, maxNum)
+}
+
+{
+  const minNum = Math.min([3, 10, 1, 6, 4])
+  const maxNum = Math.max([3, 10, 1, 6, 4])
+
+  console.log(`c.log ## minNum ##`, minNum) // NaN
+  console.log(`c.log ## maxNum ##`, maxNum) // NaN
+}
+
+{
+  const nums = [3, 10, 1, 6, 4]
+  const minNum = Math.min(...nums)
+  const maxNum = Math.max(...nums)
+
+  console.log(`c.log ## minNum ##`, minNum)
+  console.log(`c.log ## maxNum ##`, maxNum)
+}
+
+{
+  const nums = [3, 10, 1, 6, 4]
+  const minNum = Math.min.call(null, ...nums)
+  const maxNum = Math.max.apply(null, nums)
+
+  console.log(`c.log ## minNum ##`, minNum)
+  console.log(`c.log ## maxNum ##`, maxNum)
+}
+
+// bind
+{
+  const jisung = {
+    name: 'Jisung'
+  }
+
+  function update(birthYear, occupation) {
+    this.birthYear = birthYear
+    this.occupation = occupation
+  }
+
+  const updateJisung = update.bind(jisung)
+
+  updateJisung(1997, 'singer')
+
+  console.log(`c.log ## use bind jisung ##`, jisung)
+}
+
+{
+  const user = {
+    name: 'Jisung',
+    showName: function () {
+      console.log(`c.log ## hello, ${this.name} ##`)
     }
   }
 
-  functionObj.sayThis() // return this object
+  user.showName()
 
-  let arraowFunctionObj = {
-    name: 'jisung',
-    sayThis: () => {
-      console.log(`c.log ## this ##`, this)
-    }
-  }
+  let fn = user.showName
 
-  arraowFunctionObj.sayThis() // return window
+  // fn() // error - lost this
+  fn.call(user)
+  fn.apply(user)
+
+  let bindFn = fn.bind(user)
+  bindFn()
 }
 
 /**
@@ -195,6 +350,65 @@ const simpleMultiply = (a, b) => {
  ****************************************************************************************************
  */
 console.log(`\n[ IIFE(Immediately Invoked Function Expression) ]`)
-;(function hello() {
-  console.log(`IIFE`)
-})()
+
+{
+  ;(function hello() {
+    console.log(`IIFE`)
+  })()
+}
+
+/**
+ ****************************************************************************************************
+ * ### setTimeout / setInterval
+ ****************************************************************************************************
+ */
+console.log(`\n[ setTimeout/ setInterval ]`)
+
+{
+  function fn() {
+    console.log(`c.log ## 3 ##`, 3)
+  }
+
+  setTimeout(fn, 3000)
+}
+
+{
+  setTimeout(function () {
+    console.log(`c.log ## 3 ##`, 3)
+  }, 3000)
+}
+
+{
+  function showName(name) {
+    console.log(`c.log ## name ##`, name)
+  }
+
+  setTimeout(showName, 3000, 'Jisung')
+}
+
+{
+  function showName(name) {
+    console.log(`c.log ## name ##`, name)
+  }
+
+  const tId = setTimeout(showName, 3000, 'Jisung clear')
+  clearTimeout(tId)
+}
+
+{
+  function showName(name) {
+    console.log(`c.log ## name ##`, name)
+  }
+  const tId = setInterval(showName, 3000, 'Jisung')
+  clearInterval(tId)
+}
+
+// delay 0
+{
+  setTimeout(function () {
+    // Scheduling functions run after the currently executing script has finished.
+    console.log(`c.log ## test delay 0 in function ##`)
+  }, 0)
+
+  console.log(`c.log ## test delay 0 ##`)
+}

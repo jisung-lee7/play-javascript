04b87f18bdecee05ab6f89a5df0602da75d3cfac
Jisung Lee <jskurt9352@gmail.com>
2024-06-27 12:58:43 +0900

    update newES.js

--- newES.js
+++ newES.js
@@ -283,3 +283,123 @@ console.log(`\n[ Nullish coalescing operator ]`)
   const message = num ?? 'undefined'
   console.log(`c.log ## message ##`, message)
 }
+
+// ES2021
+/**
+ ****************************************************************************************************
+ * ### String.replaceAll
+ ****************************************************************************************************
+ */
+console.log(`\n[ String.replaceAll ]`)
+
+{
+  const str = 'Hello world'
+  console.log(`c.log ## str.replaceAll('l', '~') ##`, str.replaceAll('l', '~'))
+}
+
+/**
+ ****************************************************************************************************
+ * ### Promise.any
+ ****************************************************************************************************
+ */
+console.log(`\n[ Promise.any ]`)
+
+{
+  // const resPromise = new Promise((res, rej) => {
+  //   setTimeout(() => {
+  //     res('success')
+  //   }, 2000)
+  // })
+  //
+  // const rejPromise = new Promise((res, rej) => {
+  //   setTimeout(() => {
+  //     rej('fail..')
+  //   }, 1000)
+  // })
+  //
+  // // Return the object that first resolves or rejects among the Promises.
+  // Promise.race([rejPromise, resPromise])
+  //   .then(() => console.log('success'))
+  //   .catch((error) => console.log(`c.log ## error ##`, error))
+  //
+  // // Return the object that first resolves among the Promises.
+  // Promise.any([rejPromise, resPromise])
+  //   .then(() => console.log('success'))
+  //   .catch((error) => console.log(`c.log ## error ##`, error))
+}
+
+/**
+ ****************************************************************************************************
+ * ### Logical assignment Operators
+ ****************************************************************************************************
+ * ||=, &&=, ??=
+ */
+console.log(`\n[ Logical assignment Operators ]`)
+
+{
+  function add(num1, num2) {
+    // num1 ||= 0
+    // num2 ||= 0
+    console.log(`c.log ## num1 + num2 ##`, num1 + num2)
+  }
+  add()
+}
+
+/**
+ ****************************************************************************************************
+ * ### Numeric separators
+ ****************************************************************************************************
+ */
+console.log(`\n[ Numeric separators ]`)
+
+{
+  let billion = 1000000000
+  console.log(`c.log ## billion ##`, billion)
+
+  billion = 1_000_000_000
+  console.log(`c.log ## billion ##`, billion)
+}
+
+/**
+ ****************************************************************************************************
+ * ### WeakRef(Weak reference)
+ ****************************************************************************************************
+ * Avoid where possible, correct use of WeakRef takes careful thought
+ */
+console.log(`\n[ WeakRef ]`)
+{
+  // let user = { name: 'Jisung', age: 30 }
+  // const weakUser = new WeakRef(user)
+  //
+  // user = null
+  //
+  // const timer = setInterval(() => {
+  //   const wUser = weakUser.deref()
+  //   if (wUser) {
+  //     console.log(`c.log ## wUser.name ##`, wUser.name)
+  //   } else {
+  //     console.log(`c.log ## it is removed ##`)
+  //     clearInterval(timer)
+  //   }
+  // }, 1000)
+}
+
+{
+  // class MyCache {
+  //   constructor() {
+  //     this.cache = {}
+  //   }
+  //
+  //   add(key, obj) {
+  //     this.cache[key] == new WeakRef(obj)
+  //   }
+  //   get(key) {
+  //     let cachedRef = this.cache[key].deref()
+  //     if (cachedRef) {
+  //       return cachedRef
+  //     } else {
+  //       return false
+  //     }
+  //   }
+  // }
+}

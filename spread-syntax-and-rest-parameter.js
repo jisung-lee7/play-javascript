d3e9f17a32458a79721e9fc2e227b2ccc831bdd5
Jisung Lee <jskurt9352@gmail.com>
2024-09-07 19:27:43 +0900

    update spread-syntax-and-rest-parameters

---
+++ spread-syntax-and-rest-parameter.js
@@ -0,0 +1,25 @@
+/**
+ ****************************************************************************************************
+ * ### Spread Syntax
+ ****************************************************************************************************
+ */
+console.log(`\n[ Spread Syntax ]`)
+
+{
+  // // array concatenation
+  // const fruits1 = ['peach', 'strawberry']
+  // const fruits2 = ['banana', 'kiwi']
+  // const fruits = [...fruits1, ...fruits2]
+  //
+  // console.log(`c.log ## fruits ##`, fruits)
+}
+
+{
+  // // object merge
+  // // Note: If keys are the same, they will be overwritten.
+  // const dog1 = { dog1: 'dog1Value' }
+  // const dog2 = { dog2: 'dog2Value' }
+  // const dog = { ...dog1, ...dog2 }
+  //
+  // console.log(`c.log ## dog ##`, dog)
+}
